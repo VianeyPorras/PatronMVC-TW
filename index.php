bf8630f60513d667b8a592fd5a16235c2b7b2f35
Viany <vianeyporras89@gmail.com>
2026-03-23 22:25:00 -0600

    Actualizando index.php

--- index.php
+++ index.php
@@ -8,7 +8,19 @@
 // 7LD9 - wofjIs -> valor de la variable
 // & -> separador de variables 
 // %20 -> espacio en la url
-$nombre = "Neri";
-echo "Hola Mundo" +.$nombre;
-Print("Hola Mundo");
+$nombre = "Vianey";
+$edad = "24 años";
+$estatura = "1.48 cm";
+$ColorPiel = "Blanca";
+$peso = "53 kg";
+$ColorFav = "Azul";
+$Nacionalidad = "Mexicana";
+$EstadoCivil = "Soltera";
+$Nacimiento = "27/08/2002";
+$Colonia = "San Juanico";
+
+echo "Hola, mi nombre es" .$nombre "Tengo" .$edad "Mido" .$estatura "Mi color de piel es".$ColorPiel
+"Peso" .$peso."<br> Mi color favorito es"  .$ColorFav "<br> Soy de nacionalidad" .$Nacionalidad "<br>
+Estado civil:"  .$EstadoCivil "<br>Fecha de nacimiento:" .$Nacimiento "<br>
+Vivo en la colonia" .$Colonia;
 ?>
