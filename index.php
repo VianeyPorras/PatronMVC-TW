8e26955f6024d1bb03ae2a1bebcf2eeb15268619
Viany <vianeyporras89@gmail.com>
2026-03-23 22:35:11 -0600

    Corrigiendo codigo php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 // 7LD9 - wofjIs -> valor de la variable
 // & -> separador de variables 
 // %20 -> espacio en la url
+
 $nombre = "Vianey";
 $edad = "24 años";
 $estatura = "1.48 cm";
@@ -19,8 +20,7 @@ $EstadoCivil = "Soltera";
 $Nacimiento = "27/08/2002";
 $Colonia = "San Juanico";
 
-echo "Hola, mi nombre es" .$nombre "Tengo" .$edad "Mido" .$estatura "Mi color de piel es".$ColorPiel
-"Peso" .$peso."<br> Mi color favorito es"  .$ColorFav "<br> Soy de nacionalidad" .$Nacionalidad "<br>
-Estado civil:"  .$EstadoCivil "<br>Fecha de nacimiento:" .$Nacimiento "<br>
-Vivo en la colonia" .$Colonia;
+echo "Hola, mi nombre es " . $nombre . " Tengo " . $edad . " Mido " . $estatura . " Mi color de piel es " . $ColorPiel . 
+" Peso " . $peso . "<br> Mi color favorito es " . $ColorFav . "<br> Soy de nacionalidad " . $Nacionalidad . 
+"<br> Estado civil: " . $EstadoCivil . "<br> Fecha de nacimiento: " . $Nacimiento . "<br> Vivo en la colonia " . $Colonia;
 ?>
