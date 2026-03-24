bd404fd783d00ac6377451a2469ef2a384a2b573
Viany <vianeyporras89@gmail.com>
2026-03-23 20:04:26 -0600

    Practica 1.1

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+//https:// -> Protocolo
+// www.youtube.com/ ---> Dominio
+// watch -> layout
+// ? -> envio de variables por metodo get 
+// v= -> definir variables en metodo get 
+// 7LD9 - wofjIs -> valor de la variable
+// & -> separador de variables 
+// %20 -> espacio en la url
+$nombre = "Neri";
+echo "Hola Mundo" +.$nombre;
+Print("Hola Mundo");
+?>
